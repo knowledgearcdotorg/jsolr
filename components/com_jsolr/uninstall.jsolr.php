2e4498c23e89fad7802dfba5819a446b5eee150c
spauldingsmails <spauldingsmails@0f7f3a99-bec8-4741-9d36-1961318ba5d5>
2010-12-08 23:43:43 +0000

    Install/uninstall crawler in the Joomla! root directory.

--- components/com_jsolr/uninstall.jsolr.php
+++ components/com_jsolr/uninstall.jsolr.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Installation scripts.
- * 
- * @author		$LastChangedBy$
- * @package		JSolr
+ * @version		$Id$
+ * @package		Wijiti
+ * @subpackage	JSolr
  * @copyright	Copyright (C) 2010 Wijiti Pty Ltd. All rights reserved.
  * @license     This file is part of the JSolr component for Joomla!.
 
@@ -25,24 +24,32 @@
  * Please feel free to add your name and email (optional) here if you have 
  * contributed any source code changes.
  * Name							Email
- * Hayden Young					<haydenyoung@wijiti.com> 
- * 
+ * Hayden Young					<haydenyoung@wijiti.com>
  */
 
 defined('_JEXEC') or die('Restricted access');
 
-$installer = new JSolrInstaller();
-$installer->install();
+function com_uninstall()
+{
+	$uninstaller = new JSolrUninstaller();
+	$uninstaller->uninstall();
+}
 
-class JSolrInstaller
+class JSolrUninstaller
 {
 	public function __construct()
 	{
 	
 	}
 	
-	public function install()
+	public function uninstall()
 	{
-				
+		$file = JPATH_ROOT.DS."crawler.php";
+		
+		if (JFile::delete($file)) {
+			echo "<p>Crawler removed from ".JPATH_ROOT." successfully. Remove the crawler from your cron job to complete the uninstallation process.</p>";
+		} else {
+			echo "<p>Crawler failed to delete from ".JPATH_ROOT.". You will need to remove it manually from ".JPATH_ROOT.".</p>";
+		}
 	}
 }
