58f7dd71ed766e1b274b501d9ff7e75365ed7011
spauldingsmails <spauldingsmails@0f7f3a99-bec8-4741-9d36-1961318ba5d5>
2011-01-28 03:54:50 +0000

    Updated content crawl to match new schema.

--- plugins/crawler/content/jsolrcontent.php
+++ plugins/crawler/content/jsolrcontent.php
@@ -32,6 +32,7 @@
 defined('_JEXEC') or die();
 
 jimport('joomla.error.log');
+jimport('joomla.language.helper');
 
 require_once JPATH_LIBRARIES."/joomla/database/table/section.php";
 require_once JPATH_LIBRARIES."/joomla/database/table/category.php";
@@ -53,7 +54,7 @@ class plgCrawlerJSolrContent extends JPlugin
 	 * @param 	array  $config  An array that holds the plugin configuration
 	 * @since 1.5
 	 */
-	function __construct(&$subject, $config = array())
+	public function __construct(&$subject, $config = array())
 	{
 		parent::__construct($subject, $config);
 
@@ -79,7 +80,7 @@ class plgCrawlerJSolrContent extends JPlugin
 	/**
 	* Prepares an article for indexing.
 	*/
-	function _getDocument(&$article)
+	private function _getDocument(&$article)
 	{
 		$doc = new SolrInputDocument();
 		
@@ -88,34 +89,52 @@ class plgCrawlerJSolrContent extends JPlugin
 		
 		$author = JFactory::getUser($article->created_by);
 		
+		$lang = $this->_getLang($article);
+		
+		if ($lang) {
+			$lang = "_$lang";
+		}
+		
 		$doc->addField('id',  "$this->_option." . $article->id);
 		$doc->addField('created', $created->toISO8601());
 		$doc->addField('modified', $modified->toISO8601());
-		$doc->addField('title', $article->title);
-		$doc->addField('content', strip_tags($article->introtext . $article->fulltext));
-		$doc->addField('metakeywords', $article->metakey);
-		$doc->addField('metadescription', $article->metadesc);
-		$doc->addField('author', $author->get("name"));
+		$doc->addField("title$lang", $article->title);
+		$doc->addField("content$lang", strip_tags($article->introtext . $article->fulltext));
+		$doc->addField("metakeywords$lang", $article->metakey);
+		$doc->addField("metadescription$lang", $article->metadesc);
+		$doc->addField("author$lang", $author->get("name"));
 		$doc->addField('option', $this->_option);
+		
+		foreach ($this->_getTags($article, array("h1")) as $item) {
+			$doc->addField("tags_h1$lang", $item);
+		}
+
+		foreach ($this->_getTags($article, array("h2", "h3")) as $item) {
+			$doc->addField("tags_h2_h3$lang", $item);
+		}
+		
+		foreach ($this->_getTags($article, array("h4", "h5", "h6")) as $item) {
+			$doc->addField("tags_h4_h5_h6$lang", $item);
+		}		
 		
 		$section = new JTableSection(JFactory::getDBO());
 		if ($section->load($article->sectionid)) {
-			$doc->addField('section', $section->title);
+			$doc->addField("section$lang", $section->title);
 		} else {
-			$doc->addField('section', JText::_("Uncategorised"));
+			$doc->addField("section$lang", JText::_("Uncategorised"));
 		}
 		
 		$category = new JTableCategory(JFactory::getDBO());
 		if ($category->load($article->catid)) {
-			$doc->addField('category', $category->title);
+			$doc->addField("category$lang", $category->title);
 		} else {
-			$doc->addField('category', JText::_("Uncategorised"));
+			$doc->addField("category$lang", JText::_("Uncategorised"));
 		}
 		
 		return $doc;
 	}
 	
-	function _getDeleteQueryById($ids)
+	private function _getDeleteQueryById($ids)
 	{
 		$i = 0;
 		
@@ -134,9 +153,42 @@ class plgCrawlerJSolrContent extends JPlugin
 		return $query;
 	}
 	
-	function onIndex()
+	private function _getLang(&$article)
 	{
-		$query = "SELECT a.id, a.created, a.modified, a.title, a.introtext, a.fulltext, a.created_by, a.sectionid, a.catid, a.metakey, a.metadesc " .
+		$params = new JParameter($article->attribs);
+		
+		$lang = $params->get("language", JLanguageHelper::detectLanguage());
+	
+		// Language code must take the form xx-XX.
+		if (count(explode("-", $lang)) < 2) {
+			$lang = JLanguageHelper::detectLanguage();
+		}
+
+		return $lang;
+	}
+	
+	private function _getTags(&$article, $tags)
+	{	
+		$dom = new DOMDocument();
+		@$dom->loadHTML(strip_tags($article->introtext . $article->fulltext, implode(",", $tags)));
+		$dom->preserveWhiteSpace = false;
+	
+		$text = array();		
+		
+		foreach ($tags as $tag) {
+			$content = $dom->getElementsByTagname($tag);
+
+		    foreach ($content as $item) {
+	        	$text[] = $item->nodeValue;
+		    }
+		}
+
+		return $text;		
+	}
+	
+	public function onIndex()
+	{
+		$query = "SELECT a.id, a.created, a.modified, a.title, a.introtext, a.fulltext, a.created_by, a.sectionid, a.catid, a.metakey, a.metadesc, a.attribs " .
 				 "FROM #__content AS a WHERE a.state = 1 AND a.checked_out = 0;";
 		
 		$database = JFactory::getDBO();
