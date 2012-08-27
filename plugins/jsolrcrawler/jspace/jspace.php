20d1204ec46edc22afa103e15f5e6a8dbaa937b5
Hayden Young <haydenyoung@wijiti.com>
2012-08-28 00:09:19 +0800

    JSOLR-6 Index bitstreams.

--- plugins/jsolrcrawler/jspace/jspace.php
+++ plugins/jsolrcrawler/jspace/jspace.php
@@ -128,7 +128,6 @@ class plgJSolrCrawlerJSpace extends JSolrCrawlerPlugin
 				case 'contributor':
 					if ($item->qualifier == 'author') {
 						$doc->addField('author', $item->value);
-						$doc->addField('author_sort', $item->value);
 						$doc->addField('author_'.$lang, $item->value);
 					}
 					
@@ -147,6 +146,7 @@ class plgJSolrCrawlerJSpace extends JSolrCrawlerPlugin
 					
 				case 'description':
 					if (!$item->qualifier) {
+						$doc->addField('body_'.$lang, $item->value);
 						$doc->addField($field.'_'.$lang, $item->value);
 					} else {
 						$doc->addField($field.'_s_multi', $item->value); 
@@ -155,6 +155,92 @@ class plgJSolrCrawlerJSpace extends JSolrCrawlerPlugin
 				default:
 					$doc->addField($field.'_s_multi', $item->value);
 					break;
+			}
+		}
+		
+		return $doc;
+	}
+	
+	/**
+	 * Gets a list of bitstreams for the parent item.
+	 * 
+	 * @param stdClass $parent The parent Solr item.
+	 * @return array An array of bitstream objects.
+	 */
+	private function _getBitstreams($parent)
+	{
+		$bundles = array();
+		$bitstreams = array();
+
+		$params = $this->params;
+		
+		if ($params->get('use_jspace_connection_params')) {		
+			if (!JComponentHelper::isEnabled("com_jspace")) {
+				JLog::add(JText::_('PLG_JSOLRCRAWLER_JSPACE_COM_JSPACE_NOT_FOUND'), JLog::ERROR, 'jsolrcrawler');
+				return;	
+			}
+
+			$params = JComponentHelper::getParams('com_jspace');		
+		}
+		
+		$url = JFactory::getURI($params->get("rest_url").'/items/'.$parent->id.'/bundles.json?type=ORIGINAL');		
+		$url->setVar("user", $params->get("user"));
+		$url->setVar("pass", $params->get("pass"));
+
+		$client = new JRestClient($url->toString(), 'get');
+		$client->execute();
+
+		if (JArrayHelper::getValue($client->getResponseInfo(), "http_code") == 200) {
+        	$bundles = json_decode($client->getResponseBody());
+
+		} else {
+			JLog::add($client->getResponseInfo(). " " . $client->getResponseBody(), JLog::ERROR, 'jsolrcrawler');			
+		}
+
+		$i = 0;
+		
+		foreach ($bundles as $bundle) {
+			foreach ($bundle->bitstreams as $bitstream) {
+				$document = $this->_extract($params->get('base_url').'/bitstream/handle/'.$parent->handle.'/'.rawurlencode($bitstream->name));
+				$bitstreams[$i] = $bitstream;
+				$bitstreams[$i]->body = $document->body;
+				$bitstreams[$i]->metadata = $document->metadata;
+				
+				$i++;
+			}
+		}
+		
+		return $bitstreams;
+	}
+	
+	private function _getBitstreamDocument($record)
+	{
+		$doc = new Apache_Solr_Document();
+		
+		$lang = $this->getLanguage($record, false);
+
+		$doc->addField('id', $record->id);
+		$doc->addField('extension', $this->get('extension'));
+		$doc->addField('view', 'bitstream');
+		$doc->addField('lang', $this->getLanguage($record));
+		$doc->addField('key', $this->get('extension').'.bitstream.'.$record->id);
+		
+		$doc->addField('title', $record->name);
+		$doc->addField('title_'.$lang, $record->name);
+		
+		if (strip_tags($record->body)) {
+			$doc->addField("body_$lang", strip_tags($record->body));
+		}
+		
+		foreach ($record->metadata->toArray() as $key=>$value) {
+			$metakey = $this->_cleanBitstreamMetadataKey($key);
+
+			if (is_float($value)) {
+				$doc->addField($metakey.'_f_multi', $value);
+			} elseif (is_int($value) || is_long($value)) {
+				$doc->addField($metakey.'_i_multi', $value);
+			} else {
+				$doc->addField($metakey.'_s_multi', $value);	
 			}
 		}
 		
@@ -188,10 +274,52 @@ class plgJSolrCrawlerJSpace extends JSolrCrawlerPlugin
 			$documents[$i]->addField('view', $this->get('view'));
 			$documents[$i]->addField('lang', $this->getLanguage($item));
 			$documents[$i]->addField('key', $this->buildKey($documents[$i]));
-					
+
 			$ids[$i] = JArrayHelper::getValue($documents[$i]->getField('key'), 0);
+
+			// index bitstream metadata and content against record to 
+			// enhance searching. These values are for enhanced search 
+			// only and shouldn't be used when retrieving information about 
+			// an individual bitstream.
+			$bitstreams = $this->_getBitstreams($item);		
+	
+			$j=$i;
+			$j++;
+
+			foreach ($bitstreams as $bitstream) {
+				$documents[$i]->addField('bitstream_title_'.$this->getLanguage($item, false), $bitstream->name);
+				$documents[$i]->addField('bitstream_body_'.$this->getLanguage($item, false), strip_tags($bitstream->body));
+
+				foreach ($bitstream->metadata->toArray() as $key=>$value) {
+					$metakey = $this->_cleanBitstreamMetadataKey($key);
+	
+					if (is_float($value)) {
+						$documents[$i]->addField('bitstream_'.$metakey.'_f_multi', $value);
+					} elseif (is_int($value) || is_long($value)) {
+						$documents[$i]->addField('bitstream_'.$metakey.'_i_multi', $value);
+					} else {
+						$documents[$i]->addField('bitstream_'.$metakey.'_s_multi', $value);	
+					}
+				}
+				
+				$documents[$j] = $this->_getBitstreamDocument($bitstream);
+
+				if ($documents[$i]->getField('created')) {
+					$documents[$j]->addField("created", JArrayHelper::getValue(JArrayHelper::getValue($documents[$i]->getField('created'), 'value'), 0));
+				}
+				
+				if ($documents[$i]->getField('modified')) {
+					$documents[$j]->addField("modified", JArrayHelper::getValue(JArrayHelper::getValue($documents[$i]->getField('modified'), 'value'), 0));
+				}
+				
+				$documents[$j]->addField("parent_id", $item->id);
+				
+				$ids[$j] = JArrayHelper::getValue($documents[$j]->getField('key'), 0);
+				
+				$j++;
+			}
 			
-			$i++;
+			$i=$j;
 		}
 
 		try {
@@ -225,5 +353,94 @@ class plgJSolrCrawlerJSpace extends JSolrCrawlerPlugin
 	protected function buildQuery()
 	{
 		return "";
+	}
+	
+	/**
+	 * Extracts a file's contents and metadata.
+	 * 
+	 * To access the returned result's contents and metadata, use the 
+	 * properties body and metadata.
+	 * 
+	 * @example
+	 * $result = $this->_extract($path);
+	 * $body = $result->body;
+	 * $metadata = $result->metadata;
+	 * 
+	 * @param string $path
+	 * @return stdClass An object containing the file's body and metadata.
+	 */
+	private function _extract($path)
+	{
+		$params = JComponentHelper::getParams("com_jsolrindex", true);
+		
+		$document = new stdClass();
+
+		switch ($params->get('extractor')) {
+			case "local":
+				ob_start();
+				passthru("java -jar ".$params->get('local_tika_app_path')." ".$path);
+				$result = ob_get_contents();
+				ob_end_clean();				
+				$document->body = $result;
+
+				ob_start();
+				passthru("java -jar ".$params->get('local_tika_app_path')." -j ".$path);
+				$result = ob_get_contents();
+				ob_end_clean();
+
+				$document->metadata = new JRegistry();
+				$document->metadata->loadString($result);
+
+				break;
+
+			case "solr":
+				$url = $params->get('host');
+				
+				if ($params->get('username') && $params->get('password')) {
+					$url = $params->get('username') . ":" . $params->get('password') . "@" . $url;
+				}
+		
+				$solr = new Apache_Solr_Service($url, $params->get('port'), $params->get('path'));
+								
+				$extraction = $solr->extract($path, array("extractOnly"=>"true"));
+				
+				$response = json_decode($extraction->getRawResponse(), true);
+		
+				$document->content = $response[""];
+				
+				$metadata = array();
+				
+				foreach ($response->null_metadata as $key=>$value) {
+					$metadata[$key] = JArrayHelper::getValue($value, 0);
+				}
+				
+				$metadata = new JRegistry();
+				$metadata->loadArray($data);
+				
+				$document->metadata = new JRegistry();
+				$document->metadata->loadArray($metadata);
+
+				break;
+				
+			default:
+				
+				break;
+		}
+		
+		return $document;
+	}
+	
+	/**
+	 * Clean metadata key so that it is index friendly.
+	 * @param string $key The key to clean.
+	 * @return string The cleaned metadata key.
+	 */
+	private function _cleanBitstreamMetadataKey($key)
+	{
+  		$metakey = strtolower($key);
+		$metakey = preg_replace("/[^a-z0-9\s\-]/i", "", $metakey);
+		$metakey = preg_replace("/[\s\-]/", "_", $metakey);
+
+		return $metakey;
 	}
 }
