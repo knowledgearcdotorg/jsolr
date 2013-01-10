4df04a37537d9ec3976330e284799d6bf3314b1b
Hayden Young <haydenyoung@wijiti.com>
2013-01-10 19:16:24 +0800

    Updated developer documentation.

--- plugins/content/jsolrcrawler/jsolrcrawler.php
+++ plugins/content/jsolrcrawler/jsolrcrawler.php
@@ -1,14 +1,37 @@
 <?php
 /**
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package		JSolr.Plugins
+ * @subpackage	Content
+ * @copyright	Copyright (C) 2013 Wijiti Pty Ltd. All rights reserved.
+ * @license		This file is part of the JSolr Content Crawler plugin for Joomla!.
+
+   The JSolr Content Crawler plugin for Joomla! is free software: you can redistribute it 
+   and/or modify it under the terms of the GNU General Public License as 
+   published by the Free Software Foundation, either version 3 of the License, 
+   or (at your option) any later version.
+
+   The JSolr Content Crawler plugin for Joomla! is distributed in the hope that it will be 
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the JSolr Content Index plugin for Joomla!.  If not, see 
+   <http://www.gnu.org/licenses/>.
+
+ * Contributors
+ * Please feel free to add your name and email (optional) here if you have 
+ * contributed any source code changes.
+ * Name							Email
+ * Hayden Young					<haydenyoung@wijiti.com> 
+ * 
  */
 
 // No direct access.
 defined('_JEXEC') or die;
 
 /**
- * Vote plugin.
+ * A class for handling content manipulation via the com_content event model. 
  *
  * @package		JSolr.Plugins
  * @subpackage	Content
@@ -16,13 +39,12 @@ defined('_JEXEC') or die;
 class plgContentJSolrCrawler extends JPlugin
 {
 	/**
-	 * Constructor
+	 * Initializes an instance of this class.
 	 *
-	 * @access      public
 	 * @param       object  $subject The object to observe
 	 * @param       array   $config  An array that holds the plugin configuration
 	 */
-	public function __construct(& $subject, $config)
+	public function __construct(&$subject, $config)
 	{
 		parent::__construct($subject, $config);
 		$this->loadLanguage();
@@ -33,6 +55,6 @@ class plgContentJSolrCrawler extends JPlugin
 		$dispatcher = JDispatcher::getInstance();
 		JPluginHelper::importPlugin('jsolrcrawler');
 
-		$results = $dispatcher->trigger('onJSolrIndexAfterSave', array($context, $article, $isNew));		
+		$results = $dispatcher->trigger('onJSolrIndexAfterSave', array($context, $article));
 	}
 }
