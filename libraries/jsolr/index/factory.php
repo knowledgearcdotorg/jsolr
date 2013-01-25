86a6be7bb3f9be5472a4f2a12e8e48558bdb43c8
Hayden Young <haydenyoung@wijiti.com>
2013-01-25 14:05:09 +0800

    Index factory.

---
+++ libraries/jsolr/index/factory.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package		JSolr
+ * @copyright	Copyright (C) 2013 Wijiti Pty Ltd. All rights reserved.
+ * @license     This file is part of the JSolr library for Joomla!.
+
+   The JSolr library for Joomla! is free software: you can redistribute it 
+   and/or modify it under the terms of the GNU General Public License as 
+   published by the Free Software Foundation, either version 3 of the License, 
+   or (at your option) any later version.
+
+   The JSolr library for Joomla! is distributed in the hope that it will be 
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the JSolrIndex component for Joomla!.  If not, see 
+   <http://www.gnu.org/licenses/>.
+
+ * Contributors
+ * Please feel free to add your name and email (optional) here if you have 
+ * contributed any source code changes.
+ * Name							Email
+ * Hayden Young					<haydenyoung@wijiti.com> 
+ * 
+ */
+ 
+// no direct access
+defined('_JEXEC') or die();
+
+jimport('jsolr.factory');
+
+class JSolrIndexFactory extends JSolrFactory 
+{
+	protected static $component = 'com_jsolrindex';
+}
