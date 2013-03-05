ab7eb3420e14d586380f4e754d9b4f4346f320aa
Bartłomiej Kiełbasa <kontakt@bkielbasa.pl>
2013-03-05 11:07:17 +0100

    add basic classess for form fields

---
+++ libraries/jsolr/form/fields/abstract.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Abstract class for all JSolr form fields
+ *
+ * @author		$LastChangedBy: bartlomiejkielbasa $
+ * @package		JSolr
+ *
+ * @author Bartlomiej Kielbasa <bartlomiejkielbasa@wijiti.com> *
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.form.formfield');
+
+class JSolrFormAbstract extends JFormField
+{
+	/**
+	 * Returns rendered HTML form field for facet filter
+	 */
+	abstract protected function getInputFacetFilter();
+	
+	/**
+	 * Returns rendered HTML form field for search tool
+	 */
+	abstract protected function getInputSearchTool();
+	
+	/**
+	 * Returns rendered field
+	 */
+	public function getInput()
+	{
+		if ($this->form->getType() == JSolrForm::TYPE_FACETFILTERS) {
+			return $this->getInputFacetFilter();
+		}
+		
+		return $this->getInputSearchTool();
+	}
+}
---
+++ libraries/jsolr/form/fields/text.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Supports a text field
+ * 
+ * @author		$LastChangedBy: bartlomiejkielbasa $
+ * @package		JSolr
+ *
+ * @author Bartlomiej Kielbasa <bartlomiejkielbasa@wijiti.com> * * 
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('jsolr.form.fields.abstract');
+
+class JSolrFormFieldText extends JSolrFormAbstract
+{
+	protected $type = 'JSolr.Text';
+	
+	/**
+	 * @inheritdoc
+	 */
+	function getInputFacetFilter()
+	{
+		return '<input type="text" name="' . $this->name . '" value="" id="' . $this->element['name'] . '" />';
+	}
+	
+	/**
+	 * @inheritdoc
+	 */
+	function getInputSearchTool()
+	{
+		return '<input type="text" name="' . $this->name . '" value="" id="' . $this->element['name'] . '" />';
+	}
+}
