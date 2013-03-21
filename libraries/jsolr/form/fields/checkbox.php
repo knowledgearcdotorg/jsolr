628ed373fd61bed6b337e1a811dfdcaafbde0307
Bartłomiej Kiełbasa <kontakt@bkielbasa.pl>
2013-03-21 11:36:23 +0100

    isssue JSOLR-39

---
+++ libraries/jsolr/form/fields/checkbox.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Class to select lang
+ *
+ * @author      $LastChangedBy: bartlomiejkielbasa $
+ * @package     JSolr
+ *
+ * @author Bartlomiej Kielbasa <bartlomiejkielbasa@wijiti.com> *
+ */
+
+jimport('jsolr.form.fields.abstract');
+
+class JSolrFormFieldCheckbox extends JSolrFormFieldAbstract 
+{
+	public $type = 'JSolr.Checkbox';
+
+	function getInputSearchTools()
+	{
+		// Initialize some field attributes.
+		$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
+		$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
+		$checked = ((string) $this->element['value'] == $this->value) ? ' checked="checked"' : '';
+
+		// Initialize JavaScript field attributes.
+		$onclick = $this->element['onclick'] ? ' onclick="' . (string) $this->element['onclick'] . '"' : '';
+
+		return '<input type="checkbox" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
+			. htmlspecialchars((string) $this->element['value'], ENT_COMPAT, 'UTF-8') . '"' . $class . $checked . $disabled . $onclick . '/>';
+	}
+
+	function getInputFacetFilter()
+	{
+		// Initialize some field attributes.
+		$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
+		$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
+		$checked = ((string) $this->element['value'] == $this->value) ? ' checked="checked"' : '';
+
+		// Initialize JavaScript field attributes.
+		$onclick = $this->element['onclick'] ? ' onclick="' . (string) $this->element['onclick'] . '"' : '';
+
+		return '<input type="checkbox" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
+			. htmlspecialchars((string) $this->element['value'], ENT_COMPAT, 'UTF-8') . '"' . $class . $checked . $disabled . $onclick . '/>';
+	}
+
+	function getValueText()
+	{
+		return $this->getLabel();
+	}
+
+	function getFilter()
+	{
+		$facet = $this->element['facet'];
+
+		if (!empty($this->value)) {
+			return $facet . ':true';
+		}
+
+		return '';
+	}
+}
