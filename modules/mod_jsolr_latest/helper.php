6bf121ddc19eda0c9910c27fa303334c71fd60b1
Hayden Young <haydenyoung@wijiti.com>
2014-02-21 05:11:29 +0800

    JSOLR-114 Latest Items listed in a default layout.

---
+++ modules/mod_jsolr_latest/helper.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @package		JSolr.Module
+ * @copyright	Copyright (C) 2014 KnowledgeARC Ltd. All rights reserved.
+ * @license     This file is part of the JSolr Latest Items module for Joomla!.
+
+   The JSolr Latest Items module for Joomla! is free software: you can 
+   redistribute it and/or modify it under the terms of the GNU General Public 
+   License as published by the Free Software Foundation, either version 3 of 
+   the License, or (at your option) any later version.
+
+   The JSolr Latest Items module for Joomla! is distributed in the hope 
+   that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the JSolr filter module for Joomla!.  If not, see 
+   <http://www.gnu.org/licenses/>.
+
+ * Contributors
+ * Please feel free to add your name and email (optional) here if you have 
+ * contributed any source code changes.
+ * Name							Email
+ * Hayden Young					<hayden@knowledgearc.com> 
+ * 
+ */
+
+jimport('jsolr.search.factory');
+
+class ModJSolrLatestHelper
+{
+	public function getItems($params)
+	{
+		$results = array();
+
+		if (class_exists('JSolrSearchFactory')) {	
+			try {
+				$client = JSolrSearchFactory::getService();
+				
+				$query = JSolrSearchFactory::getQuery('*:*')
+					->limit($params->get('count', 5))
+					->sort($params->get('ordering', 'modified').' desc')
+					->useQueryParser("edismax")
+					->retrieveFields("*,score");
+
+				if ($limit = $params->get('o')) {
+					$query->filters('extension:'.$limit);
+				}
+	
+				if ($fq = $params->get('fq', null)) {
+					$filters = array();
+					$parts = explode(',', $fq);
+					
+					foreach ($parts as $part) {
+						$filters[] = trim($part);
+					}
+					
+					$query->mergeFilters($filters);
+				}
+
+				$results = $query->search();
+			} catch (Exception $e) {
+
+			}
+		}		
+
+		return $results;
+	}
+}
---
+++ modules/mod_jsolr_latest/mod_jsolr_latest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package		JSolr.Module
+ * @copyright	Copyright (C) 2014 KnowledgeARC Ltd. All rights reserved.
+ * @license     This file is part of the JSolr Latest Items module for Joomla!.
+
+   The JSolr Latest Items module for Joomla! is free software: you can 
+   redistribute it and/or modify it under the terms of the GNU General Public 
+   License as published by the Free Software Foundation, either version 3 of 
+   the License, or (at your option) any later version.
+
+   The JSolr Latest Items module for Joomla! is distributed in the hope 
+   that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the JSolr filter module for Joomla!.  If not, see 
+   <http://www.gnu.org/licenses/>.
+
+ * Contributors
+ * Please feel free to add your name and email (optional) here if you have 
+ * contributed any source code changes.
+ * Name							Email
+ * Hayden Young					<hayden@knowledgearc.com> 
+ * 
+ */
+
+defined('_JEXEC') or die;
+
+require_once dirname(__FILE__).'/helper.php';
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+$items = ModJSolrLatestHelper::getItems($params);
+
+require JModuleHelper::getLayoutPath('mod_'.$module->name, $params->get('layout', 'default'));
