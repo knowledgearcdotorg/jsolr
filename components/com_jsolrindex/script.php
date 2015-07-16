c1b75ddd1f1477e7df4b2ab1f8f3aa1a95ee77df
Hayden Young <hayden@knowledgearc.com>
2015-07-16 04:40:19 +0200

    Remove redundant files.

--- components/com_jsolrindex/script.php
+++ components/com_jsolrindex/script.php
@@ -2,9 +2,9 @@
 /**
  * Installation scripts.
  *
- * @package		JSolr.Index
- * @subpackage	Installer
- * @copyright	Copyright (C) 2012-2015 KnowledgeArc Ltd. All rights reserved.
+ * @package        JSolr.Index
+ * @subpackage    Installer
+ * @copyright    Copyright (C) 2012-2015 KnowledgeArc Ltd. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -15,137 +15,155 @@ jimport('joomla.filesystem.folder');
 
 class Com_JSolrIndexInstallerScript
 {
-	private $dependencies = array(
-		'libraries'=>array(
-			'jsolr'=>array(
-				'published'=>true
-			)
-		),
-		'modules'=>array(
-			'mod_jsolrconnectionmonitor'=>array(
-				'published'=>true,
-				'client_id'=>'1',
-				'position'=>'jsolrindex',
-				'title'=>'MOD_JSOLRCONNECTIONMONITOR_DEFAULT_INDEX_TITLE',
-				'params'=>'{"service":"index"}',
-				'installed_position'=>'jsolrindex' // use for replacing modules by position.
-			)
-		),
-		'plugins'=>array(
-		//	'plg_system_mysystemplugin'=>array(
-		//		'published'=>true
-		//	)
-		)
-	);
+    private $dependencies = array(
+        'libraries'=>array(
+            'jsolr'=>array(
+                'published'=>true
+            )
+        ),
+        'modules'=>array(
+            'mod_jsolrconnectionmonitor'=>array(
+                'published'=>true,
+                'client_id'=>'1',
+                'position'=>'jsolrindex',
+                'title'=>'MOD_JSOLRCONNECTIONMONITOR_DEFAULT_INDEX_TITLE',
+                'params'=>'{"service":"index"}',
+                'installed_position'=>'jsolrindex' // use for replacing modules by position.
+            )
+        ),
+        'plugins'=>array(
+        //    'plg_system_mysystemplugin'=>array(
+        //        'published'=>true
+        //    )
+        )
+    );
 
-	public function install($parent)
-	{
+    public function install($parent)
+    {
 
-	}
+    }
+    public function update(JAdapterInstance $adapter)
+    {
+        $src = $adapter->getParent()->getPath('source');
+        $site = $adapter->getParent()->getPath('extension_site');
+        $admin = $adapter->getParent()->getPath('extension_administrator');
 
-	public function update($parent)
-	{
+        $attributes = $adapter->getParent()->get('manifest')->media->attributes();
+        $attributes = reset($attributes);
 
-	}
+        $extension = \Joomla\Utilities\ArrayHelper::getValue($attributes, 'destination');
 
-	public function uninstall($parent)
-	{
-		$src = JPATH_ROOT."/cli/jsolr_crawler.php";
+        $exclude = array();
+        $exclude[] = $adapter->get('manifest_script');
+        $exclude[] = JFile::getName($adapter->getParent()->getPath('manifest'));
 
-		if (JFile::exists($src)){
-			if (JFile::delete($src)) {
-				echo "<p>Crawler uninstalled from ".$src." successfully.</p>";
-			} else {
-				echo "<p>Could not uninstall crawler from ".$src.". You will need to manually remove it.</p>";
-			}
-		}
-	}
+        $this->removeRedundantFiles($site, $src.'/site');
+        $this->removeRedundantFiles($admin, $src.'/admin', $exclude);
 
-	public function postflight($type, $parent)
-	{
-		$crawler = $this->_installCrawler($parent);
+        if ($extension) {
+            $media = JPATH_ROOT.'/media/'.$extension;
+            $this->removeRedundantFiles($media, $src.'/media', $exclude);
+        }
+    }
 
-		$dependencies = $this->_installDependencies($parent);
+    public function uninstall($parent)
+    {
+        $src = JPATH_ROOT."/cli/jsolr_crawler.php";
 
-		?>
-		<table class="adminlist table table-striped" style="width: 100%;">
-			<thead>
-				<tr>
-					<th class="title">Extension</th>
-					<th width="30%">Status</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>JSolr Crawler</td>
-					<td>
-						<?php if ($crawler) : ?>
-						<strong style="color: green">Installed</strong>
-						<?php else : ?>
-						<strong style="color: red">Not Installed</strong>
-						<?php endif; ?>
-					</td>
-				</tr>
+        if (JFile::exists($src)){
+            if (JFile::delete($src)) {
+                echo "<p>Crawler uninstalled from ".$src." successfully.</p>";
+            } else {
+                echo "<p>Could not uninstall crawler from ".$src.". You will need to manually remove it.</p>";
+            }
+        }
+    }
 
-				<?php foreach ($dependencies as $key=>$value) : ?>
-				<tr>
-					<td><?php echo $key; ?></td>
-					<td>
-						<?php if (JArrayHelper::getValue($value, 'status') == 1) : ?>
-						<strong style="color: green">Installed</strong>
-						<?php elseif (JArrayHelper::getValue($value, 'status') == 2) : ?>
-						<strong>Up-to-date</strong>
-						<?php else : ?>
-						<strong style="color: red">Not Installed</strong>
-						<?php endif; ?>
-					</td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-		<?php
-	}
+    public function postflight($type, $parent)
+    {
+        $crawler = $this->installCrawler($parent);
 
-	private function _installCrawler($parent)
-	{
-		$success = false;
+        $dependencies = $this->installDependencies($parent);
 
-		$src = $parent->getParent()->getPath('extension_administrator').'/cli/jsolr_crawler.php';
+        ?>
+        <table class="adminlist table table-striped" style="width: 100%;">
+            <thead>
+                <tr>
+                    <th class="title">Extension</th>
+                    <th width="30%">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>JSolr Crawler</td>
+                    <td>
+                        <?php if ($crawler) : ?>
+                        <strong style="color: green">Installed</strong>
+                        <?php else : ?>
+                        <strong style="color: red">Not Installed</strong>
+                        <?php endif; ?>
+                    </td>
+                </tr>
 
-		$cli = JPATH_ROOT.'/cli/jsolr_crawler.php';
+                <?php foreach ($dependencies as $key=>$value) : ?>
+                <tr>
+                    <td><?php echo $key; ?></td>
+                    <td>
+                        <?php if (JArrayHelper::getValue($value, 'status') == 1) : ?>
+                        <strong style="color: green">Installed</strong>
+                        <?php elseif (JArrayHelper::getValue($value, 'status') == 2) : ?>
+                        <strong>Up-to-date</strong>
+                        <?php else : ?>
+                        <strong style="color: red">Not Installed</strong>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <?php
+    }
 
-		if (JFile::exists($src)) {
-			if ($success = JFile::move($src, $cli)) {
-				JFolder::delete($parent->getParent()->getPath('extension_administrator').'/cli');
-			}
-		}
+    private function installCrawler($parent)
+    {
+        $success = false;
 
-		return $success;
-	}
+        $src = $parent->getParent()->getPath('extension_administrator').'/cli/jsolr_crawler.php';
 
-	/**
-	 * Installs all dependencies.
-	 *
-	 * @param JAdapterInstance $parent
-	 */
-	private function _installDependencies($parent)
-	{
-		$installed = array();
+        $cli = JPATH_ROOT.'/cli/jsolr_crawler.php';
 
-		$installer = new JInstaller();
-		$installer->setOverwrite(true);
+        if (JFile::exists($src)) {
+            if ($success = JFile::move($src, $cli)) {
+                JFolder::delete($parent->getParent()->getPath('extension_administrator').'/cli');
+            }
+        }
 
-		$src = $parent->getParent()->getPath('source');
+        return $success;
+    }
 
-		foreach ($this->dependencies as $type=>$extension) {
-			foreach ($extension as $name=>$params) {
-				$packageZip = $src.'/'.$type.'/'.$name.'.zip';
+    /**
+     * Installs all dependencies.
+     *
+     * @param JAdapterInstance $parent
+     */
+    private function installDependencies($parent)
+    {
+        $installed = array();
 
-				if (JFile::exists($packageZip)) {
+        $installer = new JInstaller();
+        $installer->setOverwrite(true);
+
+        $src = $parent->getParent()->getPath('source');
+
+        foreach ($this->dependencies as $type=>$extension) {
+            foreach ($extension as $name=>$params) {
+                $packageZip = $src.'/'.$type.'/'.$name.'.zip';
+
+                if (JFile::exists($packageZip)) {
                     if ($package = JInstallerHelper::unpack($packageZip)) {
                         $doInstall = true;
 
-                        $path = $this->_getInstalledManifest($type, $name, $params);
+                        $path = $this->getInstalledManifest($type, $name, $params);
                         $oldManifest = null;
 
                         if (JFile::exists($path)) {
@@ -154,7 +172,7 @@ class Com_JSolrIndexInstallerScript
 
                         $dir = JArrayHelper::getValue($package, 'dir').'/';
 
-                        $path = $this->_getExtractedManifest($dir, $type, $name);
+                        $path = $this->getExtractedManifest($dir, $type, $name);
                         $newManifest = $installer->parseXMLInstallFile($path);
 
                         if ($oldManifest) {
@@ -181,139 +199,162 @@ class Com_JSolrIndexInstallerScript
                         if ($success) {
                             // post installation configuration.
                             if ($type == 'modules') {
-                                $this->_configureModule($name, $params);
+                                $this->configureModule($name, $params);
                             }
                         }
 
                         JInstallerHelper::cleanupInstall($packageZip, $dir);
                     }
                 }
-			}
-		}
+            }
+        }
 
-		return $installed;
-	}
+        return $installed;
+    }
 
-	private function _getInstalledManifest($type, $name, $params)
-	{
-		$path = JPATH_ROOT;
+    private function getInstalledManifest($type, $name, $params)
+    {
+        $path = JPATH_ROOT;
 
-		switch ($type) {
-			case 'libraries':
-				$path.="/administrator/manifests/libraries/$name";
-				break;
+        switch ($type) {
+            case 'libraries':
+                $path.="/administrator/manifests/libraries/$name";
+                break;
 
-			case 'modules':
-				if (JArrayHelper::getValue($params, 'client_id') == 1) {
-					$path.="/administrator";
-				}
-				$path.="/modules/$name/$name";
-				break;
+            case 'modules':
+                if (JArrayHelper::getValue($params, 'client_id') == 1) {
+                    $path.="/administrator";
+                }
+                $path.="/modules/$name/$name";
+                break;
 
-			case 'plugins':
-				if (count($parts = explode('_', $name, 2)) == 3) {
-					$path.='/plugins/'.JArrayHelper::getValue($parts, 1).'/'.JArrayHelper::getValue($parts, 2);
-				}
+            case 'plugins':
+                if (count($parts = explode('_', $name, 2)) == 3) {
+                    $path.='/plugins/'.JArrayHelper::getValue($parts, 1).'/'.JArrayHelper::getValue($parts, 2);
+                }
 
-		}
+        }
 
-		return $path.'.xml';
-	}
+        return $path.'.xml';
+    }
 
-	private function _getExtractedManifest($path, $type, $name)
-	{
-		switch ($type) {
-			case 'libraries':
-				$path.="/$name";
-				break;
+    private function getExtractedManifest($path, $type, $name)
+    {
+        switch ($type) {
+            case 'libraries':
+                $path.="/$name";
+                break;
 
-			case 'modules':
-				$path.="/$name";
-				break;
+            case 'modules':
+                $path.="/$name";
+                break;
 
-			case 'plugins':
-				if (count($parts = explode('_', $name, 2)) == 3) {
-					$path.='/'.JArrayHelper::getValue($parts, 2);
-				}
+            case 'plugins':
+                if (count($parts = explode('_', $name, 2)) == 3) {
+                    $path.='/'.JArrayHelper::getValue($parts, 2);
+                }
 
-		}
+        }
 
-		return $path.'.xml';
-	}
+        return $path.'.xml';
+    }
 
-	/**
-	 * Configure the module using the module's params.
-	 *
-	 * @param string $name The name of the extension; E.g. mod_mymodule.
-	 * @param string $params An array of parameters that should be used to
-	 * configure the module.
-	 */
-	private function _configureModule($name, $params)
-	{
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true)
-			->select('COUNT(*)')
-			->from('#__modules')
-			->where("module = '$name'");
+    /**
+     * Configure the module using the module's params.
+     *
+     * @param string $name The name of the extension; E.g. mod_mymodule.
+     * @param string $params An array of parameters that should be used to
+     * configure the module.
+     */
+    private function configureModule($name, $params)
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true)
+            ->select('COUNT(*)')
+            ->from('#__modules')
+            ->where("module = '$name'");
 
-		if ($clientId = JArrayHelper::getValue($params, 'client_id')) {
-			$query->where("client_id = $clientId");
-		}
+        if ($clientId = JArrayHelper::getValue($params, 'client_id')) {
+            $query->where("client_id = $clientId");
+        }
 
 
-		if ($installedPosition = JArrayHelper::getValue($params, 'installed_position')) {
-			$query->where("position = '$installedPosition'");
-		}
+        if ($installedPosition = JArrayHelper::getValue($params, 'installed_position')) {
+            $query->where("position = '$installedPosition'");
+        }
 
-		$count = $db->setQuery($query)->loadResult();
+        $count = $db->setQuery($query)->loadResult();
 
-		if (!$count) {
-			$language = JFactory::getLanguage();
-			$language->load($name, JPATH_ADMINISTRATOR, null, true);
+        if (!$count) {
+            $language = JFactory::getLanguage();
+            $language->load($name, JPATH_ADMINISTRATOR, null, true);
 
-			// Set up module per config preferences.
-			if (count($params)) {
-				$query = $db->getQuery(true)
-					->update($db->qn('#__modules'))
-					->where($db->qn('module').' = '.$db->q($name));
+            // Set up module per config preferences.
+            if (count($params)) {
+                $query = $db->getQuery(true)
+                    ->update($db->qn('#__modules'))
+                    ->where($db->qn('module').' = '.$db->q($name));
 
-				if ($title = JArrayHelper::getValue($params, 'title')) {
-					$query->set($db->qn('title').' = '.$db->q(JText::_($title)));
-				}
+                if ($title = JArrayHelper::getValue($params, 'title')) {
+                    $query->set($db->qn('title').' = '.$db->q(JText::_($title)));
+                }
 
-				if ($position = JArrayHelper::getValue($params, 'position')) {
-					$query->set($db->qn('position').' = '.$db->q($position));
-				}
+                if ($position = JArrayHelper::getValue($params, 'position')) {
+                    $query->set($db->qn('position').' = '.$db->q($position));
+                }
 
-				if(JArrayHelper::getValue($params, 'published')) {
-					$query->set($db->qn('published').' = '.$db->q('1'));
-				}
+                if(JArrayHelper::getValue($params, 'published')) {
+                    $query->set($db->qn('published').' = '.$db->q('1'));
+                }
 
-				$db->setQuery($query);
-				$db->execute();
-			}
+                $db->setQuery($query);
+                $db->execute();
+            }
 
-			// Make accessible on every page.
-			$query = $db->getQuery(true)
-				->select('id')
-				->from($db->qn('#__modules'))
-				->where($db->qn('module').' = '.$db->q($name));
+            // Make accessible on every page.
+            $query = $db->getQuery(true)
+                ->select('id')
+                ->from($db->qn('#__modules'))
+                ->where($db->qn('module').' = '.$db->q($name));
 
-			$db->setQuery($query);
-			$moduleId = $db->loadResult();
+            $db->setQuery($query);
+            $moduleId = $db->loadResult();
 
-			$query = $db->getQuery(true)
-				->select('*')
-				->from($db->qn('#__modules_menu'))
-				->where($db->qn('moduleid').' = '.$db->q($moduleId));
+            $query = $db->getQuery(true)
+                ->select('*')
+                ->from($db->qn('#__modules_menu'))
+                ->where($db->qn('moduleid').' = '.$db->q($moduleId));
 
-			$db->setQuery($query);
-			$assignments = $db->loadObjectList();
+            $db->setQuery($query);
+            $assignments = $db->loadObjectList();
 
-			if(!$assignments) {
-				$object = (object)array('moduleid'=>$moduleId, 'menuid'=>0);
-				$db->insertObject('#__modules_menu', $object);
-			}
-		}
-	}
+            if(!$assignments) {
+                $object = (object)array('moduleid'=>$moduleId, 'menuid'=>0);
+                $db->insertObject('#__modules_menu', $object);
+            }
+        }
+    }
+
+    /**
+     * Removes redundant files and directories from previous versions that no
+     * longer apply to the new version.
+     */
+    private function removeRedundantFiles($oldPath, $newPath, $exclude = array())
+    {
+        foreach (JFolder::files($oldPath, '.', true, true, $exclude) as $file) {
+            if (JFile::exists($file)) {
+                if (!JFile::exists(str_replace($oldPath, $newPath, $file))) {
+                    Jfile::delete($file);
+                }
+            }
+        }
+
+        foreach (JFolder::folders($oldPath, '.', true, true, $exclude) as $folder) {
+            if (JFolder::exists($folder)) {
+                if (!JFolder::exists(str_replace($oldPath, $newPath, $folder))) {
+                    JFolder::delete($folder);
+                }
+            }
+        }
+    }
 }
--- components/com_jsolrsearch/script.php
+++ components/com_jsolrsearch/script.php
@@ -2,31 +2,10 @@
 /**
  * Installation scripts.
  *
- * @package		JSolr.Search
- * @subpackage	Installer
+ * @package     JSolr.Search
+ * @subpackage  Installer
  * @copyright   Copyright (C) 2012-2015 KnowledgeArc Ltd. All rights reserved.
- * @license     This file is part of the JSolrSearch component for Joomla!.
-
-   The JSolrSearch component for Joomla! is free software: you can redistribute it
-   and/or modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation, either version 3 of the License,
-   or (at your option) any later version.
-
-   The JSolrSearch component for Joomla! is distributed in the hope that it will be
-   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with the JSolrSearch component for Joomla!.  If not, see
-   <http://www.gnu.org/licenses/>.
-
- * Contributors
- * Please feel free to add your name and email (optional) here if you have
- * contributed any source code changes.
- * Name							Email
- * Hayden Young					<hayden@knowledgearc.com>
- *
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die('Restricted access');
@@ -37,82 +16,50 @@ jimport('joomla.filesystem.file');
 
 class com_JSolrSearchInstallerScript
 {
-	public function install(JAdapterInstance $adapter)
-	{
-/*
- * @todo Implement package installer to install complimentary plugins?
-		$installer = new JInstaller();
-		$installer->_overwrite = true;
+    public function update(JAdapterInstance $adapter)
+    {
+        $src = $adapter->getParent()->getPath('source');
+        $site = $adapter->getParent()->getPath('extension_site');
+        $admin = $adapter->getParent()->getPath('extension_administrator');
 
-		$pkg_path = JPATH_ADMINISTRATOR.'/components/'.self::COM_JSOLRSEARCH.'/extensions/';
+        $attributes = $adapter->getParent()->get('manifest')->media->attributes();
+        $attributes = reset($attributes);
 
-		if ($handle = opendir($pkg_path)) {
-			while ($pkg = readdir($handle)) {
-				if ($pkg != "." && $pkg != ".." && $pkg != "index.html") {
-					$package = JInstallerHelper::unpack($pkg_path.$pkg);
+        $extension = \Joomla\Utilities\ArrayHelper::getValue($attributes, 'destination');
 
-					if ($installer->install($package['dir'])) {
-						$msgcolor = "#E0FFE0";
-						$msgtext  = "$pkg successfully installed.";
-					} else {
-						$msgcolor = "#FFD0D0";
-						$msgtext  = "ERROR: Could not install $pkg. Please install manually.";
-					}
-					?>
+        $exclude = array();
+        $exclude[] = $adapter->get('manifest_script');
+        $exclude[] = JFile::getName($adapter->getParent()->getPath('manifest'));
 
-					<table bgcolor="<?php echo $msgcolor; ?>" width ="100%">
-						<tr style="height:30px">
-							<td width="50px"><img src="/administrator/images/tick.png" height="20px" width="20px"></td>
-							<td><font size="2"><b><?php echo $msgtext; ?></b></font></td>
-						</tr>
-					</table>
+        $this->removeRedundantFiles($site, $src.'/site');
+        $this->removeRedundantFiles($admin, $src.'/admin', $exclude);
 
-					<?php
-					JInstallerHelper::cleanupInstall($pkg_path.$pkg, $package['dir']);
-				}
-			}
-		}
- */
-	}
+        if ($extension) {
+            $media = JPATH_ROOT.'/media/'.$extension;
+            $this->removeRedundantFiles($media, $src.'/media', $exclude);
+        }
+    }
 
-	public function update(JAdapterInstance $adapter)
-	{
-		$src = $adapter->getParent()->getPath('source');
-		$site = $adapter->getParent()->getPath('extension_site');
-		$admin = $adapter->getParent()->getPath('extension_administrator');
+    /**
+     * Removes redundant files and directories from previous versions that no
+     * longer apply to the new version.
+     */
+    private function removeRedundantFiles($oldPath, $newPath, $exclude = array())
+    {
+        foreach (JFolder::files($oldPath, '.', true, true, $exclude) as $file) {
+            if (JFile::exists($file)) {
+                if (!JFile::exists(str_replace($oldPath, $newPath, $file))) {
+                    Jfile::delete($file);
+                }
+            }
+        }
 
-		$extension = JArrayHelper::getValue($adapter->getParent()->get('manifest')->media, 'destination');
-		$extension = JArrayHelper::getValue($extension, 0);
-
-		$media = JPATH_ROOT.'/media/'.$extension;
-
-		$this->_cleanFiles($site, $src.'/site');
-
-		$exclude = array();
-		$exclude[] = $adapter->get('manifest_script');
-		$exclude[] = JFile::getName($adapter->getParent()->getPath('manifest'));
-
-		$this->_cleanFiles($admin, $src.'/admin', $exclude);
-
-		$this->_cleanFiles($media, $src.'/media', $exclude);
-	}
-
-	private function _cleanFiles($path1, $path2, $exclude = array())
-	{
-		foreach (JFolder::files($path1, '.', true, true, $exclude) as $file) {
-			if (JFile::exists($file)) {
-				if (!JFile::exists(str_replace($path1, $path2, $file))) {
-					Jfile::delete($file);
-				}
-			}
-		}
-
-		foreach (JFolder::folders($path1, '.', true, true, $exclude) as $folder) {
-			if (JFolder::exists($folder)) {
-				if (!JFolder::exists(str_replace($path1, $path2, $folder))) {
-					JFolder::delete($folder);
-				}
-			}
-		}
-	}
+        foreach (JFolder::folders($oldPath, '.', true, true, $exclude) as $folder) {
+            if (JFolder::exists($folder)) {
+                if (!JFolder::exists(str_replace($oldPath, $newPath, $folder))) {
+                    JFolder::delete($folder);
+                }
+            }
+        }
+    }
 }
