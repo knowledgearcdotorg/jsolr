451cb08e8d03929dc12766917405a91ad2337756
Hayden Young <hayden@knowledgearc.com>
2015-08-06 01:28:03 +0200

    Use PSR-compliant lib include.

--- modules/mod_jsolrfilter/helper.php
+++ modules/mod_jsolrfilter/helper.php
@@ -1,57 +1,37 @@
 <?php
 /**
- * @package		JSolr.Module
- * @copyright	Copyright (C) 2011-2013 KnowledgeARC Ltd. All rights reserved.
- * @license     This file is part of the JSolr filter module for Joomla!.
-
-   The JSolr filter module for Joomla! is free software: you can 
-   redistribute it and/or modify it under the terms of the GNU General Public 
-   License as published by the Free Software Foundation, either version 3 of 
-   the License, or (at your option) any later version.
-
-   The JSolr filter module for Joomla! is distributed in the hope 
-   that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
-   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with the JSolr filter module for Joomla!.  If not, see 
-   <http://www.gnu.org/licenses/>.
-
- * Contributors
- * Please feel free to add your name and email (optional) here if you have 
- * contributed any source code changes.
- * Name							Email
- * Hayden Young					<hayden@knowledgearc.com> 
- * Michał Kocztorz				<michalkocztorz@wijiti.com> 
- * 
+ * A helper for the filter module.
+ *
+ * @package     JSolr.Module
+ * @copyright   Copyright (C) 2011-2015 KnowledgeArc Ltd. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-jimport('jsolr.form.form');
+use \JSolr\Form\Form;
 
 class modJSolrFilterHelper
-{	
-	public static function showFilter() 
-	{
-		$form = JSolrForm::getInstance('com_jsolrsearch.search');
-		
-		$show = false;
-		
-		if (count($form->getFieldset('facets'))) {
-			if ($form->isFiltered() || JFactory::getApplication()->input->get("q", null, "string")) {
-				$show = true;
-			}
-		}
-		
-		return $show;
-	}
-	
-	/**
-	 * 
-	 * @return JSolrForm
-	 */
-	public static function getForm() 
-	{
-		return JSolrForm::getInstance('com_jsolrsearch.search');
-	}
+{
+    public static function showFilter()
+    {
+        $form = Form::getInstance('com_jsolrsearch.search');
+
+        $show = false;
+
+        if (count($form->getFieldset('facets'))) {
+            if ($form->isFiltered() || JFactory::getApplication()->input->get("q", null, "string")) {
+                $show = true;
+            }
+        }
+
+        return $show;
+    }
+
+    /**
+     *
+     * @return JSolrForm
+     */
+    public static function getForm()
+    {
+        return Form::getInstance('com_jsolrsearch.search');
+    }
 }
--- modules/mod_jsolrfilter/mod_jsolrfilter.php
+++ modules/mod_jsolrfilter/mod_jsolrfilter.php
@@ -4,35 +4,37 @@
  * @copyright	Copyright (C) 2012 KnowledgeARC Ltd. All rights reserved.
  * @license     This file is part of the JSolr filter module for Joomla!.
 
-   The JSolr filter module for Joomla! is free software: you can 
-   redistribute it and/or modify it under the terms of the GNU General Public 
-   License as published by the Free Software Foundation, either version 3 of 
+   The JSolr filter module for Joomla! is free software: you can
+   redistribute it and/or modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation, either version 3 of
    the License, or (at your option) any later version.
 
-   The JSolr filter module for Joomla! is distributed in the hope 
-   that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+   The JSolr filter module for Joomla! is distributed in the hope
+   that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with the JSolr filter module for Joomla!.  If not, see 
+   along with the JSolr filter module for Joomla!.  If not, see
    <http://www.gnu.org/licenses/>.
 
  * Contributors
- * Please feel free to add your name and email (optional) here if you have 
+ * Please feel free to add your name and email (optional) here if you have
  * contributed any source code changes.
  * Name							Email
- * Hayden Young					<hayden@knowledgearc.com> 
- * 
+ * Hayden Young					<hayden@knowledgearc.com>
+ *
  */
 
 defined('_JEXEC') or die;
+
+JLoader::registerNamespace('JSolr', JPATH_PLATFORM);
 
 require_once dirname(__FILE__).'/helper.php';
 
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
-// Don't show the filter module contents unless the user has specified 
+// Don't show the filter module contents unless the user has specified
 // something to search for.
 if (modJSolrFilterHelper::showFilter()) {
 	$form = modJSolrFilterHelper::getForm();
